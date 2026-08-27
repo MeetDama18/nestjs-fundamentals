2dc917da3eb75e0e7ce6d0469ea1fe0f3054a4da
Meet Dama <meetdama018@gmail.com>
2026-08-27 13:26:32 +0530

    Configure app for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +11,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
